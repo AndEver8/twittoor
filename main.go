d9e0e37d831b58c2ff47392a71fcc441517aa5bf
Ever Andres Jimenez Sanchez <ejimenez@lifemiles.loc>
2022-09-09 08:21:10 -0600

    Adding Manejadores and ChequeoConection functions in main function

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main(){
-	
+import (
+	"log"
+
+	"github.com/AndEver8/twittoor/bd"
+	"github.com/AndEver8/twittoor/handlers"
+)
+
+func main() {
+	if bd.ChequeoConection() == 0 {
+		log.Fatal("Sin conexion a la BD")
+		return
+	}
+
+	handlers.Manejadores()
 }
